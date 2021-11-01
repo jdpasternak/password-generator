f8247284e141acebfbd9bb1faf3812c63d878828
Jake Pasternak <jdp.pasternak@gmail.com>
2021-11-01 10:55:07 -1000

    Complete MVP

--- script.js
+++ script.js
@@ -1,4 +1,105 @@
 // Assignment code here
+var generatePassword = function () {
+  // Criteria:
+  // - lowercase, uppercase, numeric, and/or special characters
+  // - Length of 8-128 characters
+
+  // Password Length
+  var passwordLength = parseInt(
+    prompt("Enter a password length from 8 to 128 characters.")
+  );
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("ERROR: Must enter a value between 8 and 128.");
+    generatePassword();
+  }
+
+  // Password Character Criteria
+  // var hasLowercase = confirm("Include lowercase letters?");
+  // var hasUppercase = confirm("Include uppercase letters?");
+  // var hasNumbers = confirm("Include numbers?");
+  // var hasSpecial = confirm("Include special characters?");
+
+  // randomly select a character type (lower, upper, number, special)
+  // then randomly select a character from that type (ex: number 1-9; lower a-z; upper A-Z; special \u0020 - \u007e)
+
+  // ALL: 32 - 126
+  
+  // Lowercase Alphabet: 97 - 122
+
+  // Uppercase Alphabet: 65 - 90
+
+  // Numbers: 48 - 57
+
+  // Special Characters: 32 - 47, 58 - 64, 91 - 96, 123 - 126
+  var specialCharacters = [
+    " ",
+    "!",
+    '"',
+    "#",
+    "$",
+    "%",
+    "&",
+    "'",
+    "(",
+    ")",
+    "*",
+    "+",
+    ",",
+    "-",
+    ".",
+    "/",
+    ":",
+    ";",
+    "<",
+    "=",
+    ">",
+    "?",
+    "@",
+    "[",
+    "\\",
+    "]",
+    "^",
+    "_",
+    "`",
+    "{",
+    "|",
+    "}",
+    "~",
+  ];
+
+  var passwordText = "";
+  var selection = 0;
+  for (i = 0; i < passwordLength; i++) {
+    // debugger;
+    var characterType = Math.floor(Math.random() * 4);
+
+    switch (characterType) {
+      case 0:
+        // selection = Math.floor(Math.random() * alphabetLower.length);
+        selection = Math.floor(Math.random() * (122 - 97 + 1) + 97);
+        passwordText += String.fromCharCode(selection);
+        break;
+      case 1:
+        // selection = Math.floor(Math.random() * alphabetUpper.length);
+        selection = Math.floor(Math.random() * (90 - 65 + 1) + 65);
+        passwordText += String.fromCharCode(selection);
+        break;
+      case 2:
+        // selection = Math.floor(Math.random() * numbers.length);
+        selection = Math.floor(Math.random() * (57 - 48 + 1) + 48);
+        passwordText += String.fromCharCode(selection);
+        break;
+      case 3:
+        selection = Math.floor(Math.random() * specialCharacters.length);
+        passwordText += specialCharacters[selection];
+        break;
+      default:
+        break;
+    }
+  }
+
+  return passwordText;
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
