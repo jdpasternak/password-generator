0dded502aa8b720fd9543dc08a18249909d9feee
Jake Pasternak <jdp.pasternak@gmail.com>
2021-11-29 16:33:39 -1000

    Update getPasswordCriteria function

--- script.js
+++ script.js
@@ -87,45 +87,40 @@ var generatePassword = function () {
 };
 
 var getPasswordLength = function () {
-  // Prompt the user for desired password legnth
-  var passwordLength = parseInt(
-    prompt("Enter a password length from 8 to 128 characters.")
-  );
-
-  // If the password does not satisfy the length requirements, or is NaN, re-run the function
-  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
-    alert("ERROR: Must enter a value between 8 and 128.");
-    return getPasswordLength();
-  }
+  var passwordLength = $(`input[name="length"]`).val();
 
   return passwordLength;
 };
 
 var getPasswordCriteria = function () {
   // Confirm with user each of the desired criteria
+  /*  
   var hasLowercase = confirm("Include lowercase letters?");
   var hasUppercase = confirm("Include uppercase letters?");
   var hasNumbers = confirm("Include numbers?");
-  var hasSpecial = confirm("Include special characters?");
+  var hasSpecial = confirm("Include special characters?"); 
+  */
 
   // Make a list with a number for each criteria select (ex: 1,2,4 for lower, upper, and special)
+
   var passwordCriteria = [];
-  if (hasLowercase) {
+  if ($(`input[name="lowercase"]`).prop("checked")) {
     passwordCriteria.push(0);
   }
-  if (hasUppercase) {
+  if ($(`input[name="uppercase"]`).prop("checked")) {
     passwordCriteria.push(1);
   }
-  if (hasNumbers) {
+  if ($(`input[name="numbers"]`).prop("checked")) {
     passwordCriteria.push(2);
   }
-  if (hasSpecial) {
+  if ($(`input[name="characters"]`).prop("checked")) {
     passwordCriteria.push(3);
   }
+
   // If no criteria are selected, re-run the function
   if (passwordCriteria.length === 0) {
     alert("ERROR: You must select at least 1 of the criteria.");
-    return getPasswordCriteria();
+    return;
   }
 
   return passwordCriteria;
@@ -144,3 +139,23 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+/* New Feature with Form Inputs */
+/* 
+  WHEN button to Generate Password is clicked, THEN get criteria from range and checkbox inputs.
+  WHEN criteria has been received, THEN iterate to generate a random letter, number, or character for the length of the password.
+  WHEN a character is generated, THEN append it to a string.
+  WHEN finished interating, THEN display the complete generated password in the textarea.
+*/
+/* 
+  Pseudo Code
+  - Button is clicked
+  - Criteria is built by checking all checkboxes and range input
+  - Criteria is sent to function that generates password
+  - Password is generated and displayed in textarea.
+*/
+/*
+  Additional Features
+  - Click to copy to clipboard
+  - Prompt for a name for the password to save in localStorage / otherwise
+*/
