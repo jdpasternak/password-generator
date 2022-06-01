80201df590f22a3d8ed7a32c243f734e88da4075
Jake Pasternak <jdp.pasternak@gmail.com>
2022-05-31 21:14:18 -0400

    adds create-react-app template, clears defaults, installs mui

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+function App() {
+  return (
+    <Typography variant="h1" component="h1" sx={{ textAlign: "center" }}>
+      Password Generator
+    </Typography>
+  );
+}
+
+export default App;
