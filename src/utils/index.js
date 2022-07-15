df860b949035536a7f76b5d2dac0aa48e488f1c3
Jake Pasternak <jdp.pasternak@gmail.com>
2022-07-15 13:19:17 -0400

    modify generatePassword function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,15 +1,10 @@
-$(document).ready(function () {
-  $(".modal").modal();
-});
-
-var generatePassword = function () {
+export const generatePassword = function (constraints) {
   // Get password length
-  var passwordLength = parseInt(getPasswordLength());
+  var passwordLength = constraints.passwordLength;
 
   // Get password Criteria
-  var passwordCriteria = getPasswordCriteria();
+  var passwordCriteria = getPasswordCriteria(constraints);
   if (passwordCriteria.length === 0) {
-    $(".modal").modal("open");
     return false;
   }
 
@@ -58,7 +53,7 @@ var generatePassword = function () {
   // Randomly select a type from one of the chosen criteria, then randomly select a character from that type
   var passwordText = "";
   var selection = 0;
-  for (i = 0; i < passwordLength; i++) {
+  for (let i = 0; i < passwordLength; i++) {
     // Keep running this switch statement until randomly landing on one of the selected criteria
     var selected = false;
     while (!selected) {
@@ -91,79 +86,21 @@ var generatePassword = function () {
   return passwordText;
 };
 
-var copyPassword = function () {
-  $("#password").focus().select();
-  document.execCommand("copy");
-  M.toast({ html: "Copied to clipboard", displayLength: 2000 });
-};
-
-var getPasswordLength = function () {
-  var passwordLength = $(`input[name="length"]`).val();
-
-  return passwordLength;
-};
-
-var getPasswordCriteria = function () {
+const getPasswordCriteria = function (constraints) {
   // Make a list with a number for each criteria select (ex: 1,2,4 for lower, upper, and special)
   var passwordCriteria = [];
-  if ($(`input[name="lowercase"]`).prop("checked")) {
+  if (constraints.hasLowercase) {
     passwordCriteria.push(0);
   }
-  if ($(`input[name="uppercase"]`).prop("checked")) {
+  if (constraints.hasUppercase) {
     passwordCriteria.push(1);
   }
-  if ($(`input[name="numbers"]`).prop("checked")) {
+  if (constraints.hasNumbers) {
     passwordCriteria.push(2);
   }
-  if ($(`input[name="characters"]`).prop("checked")) {
+  if (constraints.hasSpecialCharacters) {
     passwordCriteria.push(3);
   }
 
   return passwordCriteria;
 };
-
-// Get references to the #generate element
-var generateBtn = $("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  if (!password) {
-    return false;
-  }
-  var passwordText = $("#password");
-  var copyBtn = $("<i></i>")
-    .addClass("material-icons prefix tooltipped")
-    .text("content_copy")
-    .attr("data-position", "top")
-    .attr("data-tooltip", "Copy to clipboard")
-    .on("click", copyPassword);
-
-  $(".input-field").prepend(copyBtn);
-  $(".tooltipped").tooltip();
-
-  passwordText.val(password);
-}
-
-// Add event listener to generate button
-generateBtn.on("click", writePassword);
-
-/* New Feature with Form Inputs */
-/* 
-  WHEN button to Generate Password is clicked, THEN get criteria from range and checkbox inputs.
-  WHEN criteria has been received, THEN iterate to generate a random letter, number, or character for the length of the password.
-  WHEN a character is generated, THEN append it to a string.
-  WHEN finished interating, THEN display the complete generated password in the textarea.
-*/
-/* 
-  Pseudo Code
-  - Button is clicked
-  - Criteria is built by checking all checkboxes and range input
-  - Criteria is sent to function that generates password
-  - Password is generated and displayed in textarea.
-*/
-/*
-  Additional Features
-  - Click to copy to clipboard
-  - Prompt for a name for the password to save in localStorage / otherwise
-*/
